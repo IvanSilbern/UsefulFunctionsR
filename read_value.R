71d5849380d37ad3cc959f6be0563e151889a797
IvanSilbern <ivansilbern@gmail.com>
2017-08-05 09:12:14 +0200

    Update read_value.R

--- read_value.R
+++ read_value.R
@@ -1,6 +1,13 @@
-read_value<-function(prompt_text = "", prompt_suffix = getOption("prompt"), coerce_to="character")
+ReadValue <- function(prompt_text = "", prompt_suffix = getOption("prompt"), coerce_to="character")
+  # Function reads input from user
+  #
+  # Args:
+  # prompt_text: Text to be shown to user.
+  # prompt_suffix: A non-empty string to be used for R's prompt.
+  # coerce_to: String specifying the type the input will be coerced to.
+  #
 {
-  prompt <-paste(prompt_text, prompt_suffix)
-  answer<-as(readline(prompt), coerce_to)
+  prompt <- paste(prompt_text, prompt_suffix)
+  answer <- as(readline(prompt), coerce_to)
   print(answer)
 }
