69d9795bc9444f472b4c945a74a2f8363f9a243d
IvanSilbern <ivansilbern@gmail.com>
2018-07-05 14:36:56 +0200

    Update SmallFunctions.R

--- SmallFunctions.R
+++ SmallFunctions.R
@@ -62,4 +62,21 @@ write.delim <- function(...){
   
   write.table(sep = "\t", dec = ".", row.names = F,  quote = F, ...)
   
+}
+        
+gm_mean = function(x, na.rm=TRUE, zero.propagate = FALSE){
+#geometric mean function
+#taken from https://stackoverflow.com/questions/2602583/geometric-mean-is-there-a-built-in
+
+  if(any(x < 0, na.rm = TRUE)){
+    return(NaN)
+  }
+  if(zero.propagate){
+    if(any(x == 0, na.rm = TRUE)){
+      return(0)
+    }
+    exp(mean(log(x), na.rm = na.rm))
+  } else {
+    exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+  }
 }        
